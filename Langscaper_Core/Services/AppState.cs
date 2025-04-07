c4db71e269c2f260e83c3c1f8ea5c44c35397362
KrankerApfel <t.rakoto76@gmail.com>
2025-04-07 17:09:40 +0200

    add AppState

---
+++ Langscaper_Core/Services/AppState.cs
@@ -0,0 +1,10 @@
+﻿using Langscaper_Core.Models;
+
+namespace Langscaper_Core.Services
+{
+
+    public class AppState
+    {
+        public LanguageModel CurrentLanguage { get; set; } = new();
+    }
+}
